a1376e5169f3767bee9885fb205343306177ec3b
shingo-cc <highlifejpn@gmail.com>
2026-08-24 20:42:30 +0900

    chore: remove temporary book name check (#479)
    Co-authored-by: shingo-ops <shingo@treasureislandjp.com>

--- src/30_TemporaryConfiguredBookNameCheck.js
+++
@@ -1,9 +0,0 @@
-/**
- * 一時確認用。Script Properties の値は返却・ログ出力しない。
- */
-function getConfiguredSpreadsheetFileNamesForVerification() {
-  return {
-    devFileName: DriveApp.getFileById(getRequiredSpreadsheetProperty('DEV_SPREADSHEET_ID')).getName(),
-    erpFileName: DriveApp.getFileById(getRequiredSpreadsheetProperty('ERP_SPREADSHEET_ID')).getName()
-  };
-}
